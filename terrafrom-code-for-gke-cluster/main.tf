9e3146f63ebe59a43db7e97a34a4f968d740eb98
tarini85 <tarinipbarik@gmail.com>
2026-02-21 15:26:34 +0530

    Update main.tf

--- terrafrom-code-for-gke-cluster/main.tf
+++ terrafrom-code-for-gke-cluster/main.tf
@@ -1,1 +1,42 @@
+# Create VPC
+resource "google_compute_network" "vpc" {
+  name                    = "gke-vpc"
+  auto_create_subnetworks = false
+}
 
+# Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "gke-subnet"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+# Create GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  ip_allocation_policy {}
+}
+
+# Create Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
