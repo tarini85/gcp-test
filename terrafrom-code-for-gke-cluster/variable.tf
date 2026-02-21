d0a88bdc3c11b711b29df4fbcf4274264a768bd5
tarini85 <tarinipbarik@gmail.com>
2026-02-21 15:27:27 +0530

    Update variable.tf
    variable file updated

--- terrafrom-code-for-gke-cluster/variable.tf
+++ terrafrom-code-for-gke-cluster/variable.tf
@@ -1,1 +1,22 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
 
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "GKE cluster name"
+  type        = string
+  default     = "my-gke-cluster"
+}
+
+variable "node_count" {
+  description = "Number of nodes"
+  type        = number
+  default     = 2
+}
